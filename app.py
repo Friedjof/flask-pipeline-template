b4cfa7f172ef1226f3cefc38168e0f5215cb5a94
Friedjof <friedjof@noweck.info>
2023-06-24 15:28:55 +0200

    update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World! Updated'
+    return "<h1>Flask App</h1>"
 
 
 if __name__ == '__main__':
